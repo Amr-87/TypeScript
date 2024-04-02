bc1373557755363ffd8fa8d562d40f83460e8458
Amr Gamal <amr.aboulela97@gmail.com>
2024-04-03 00:34:32 +0200

    modules

--- lab.js
+++ lab.js
@@ -1,18 +1,12 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
+"use strict";
+//Main
+Object.defineProperty(exports, "__esModule", { value: true });
+//!6
+var Employee_1 = require("./classes/Employee");
+var Manager_1 = require("./classes/Manager");
+var Account_1 = require("./classes/Account");
+var Current_Account_1 = require("./classes/Current_Account");
+var Saving_Account_1 = require("./classes/Saving_Account");
 //!1
 var nArray;
 nArray = [3, 4, 5];
@@ -26,24 +20,7 @@ var nboolvar = 5;
 nboolvar = true;
 //!4
 var greeting = function (name1, name2) { return console.log("hello ".concat(name1, " , hello ").concat(name2)); };
-var Employee = /** @class */ (function () {
-    function Employee(emp) {
-        this.id = emp.id;
-        this.name = emp.name;
-        this._username = emp.username;
-        this.email = emp.email;
-        this.address = emp.address;
-    }
-    Object.defineProperty(Employee.prototype, "username", {
-        get: function () {
-            return this._username;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return Employee;
-}());
-;
+//greeting('aaa','bbb');
 var EmployeeData = {
     id: 1,
     name: "Leanne Graham",
@@ -55,71 +32,18 @@ var EmployeeData = {
         zipcode: "92998-3874",
         geo: { lat: "-37.3159", lng: "81.1496" } }
 };
-var emp1 = new Employee(EmployeeData);
-//console.log(emp1);
-//!6
-var Manager = /** @class */ (function (_super) {
-    __extends(Manager, _super);
-    function Manager(manager) {
-        return _super.call(this, manager) || this;
-    }
-    Manager.prototype.GetAddress = function () {
-        return this.address;
-    };
-    return Manager;
-}(Employee));
-var man1 = new Manager(EmployeeData);
-var Account = /** @class */ (function () {
-    function Account(ano, balance, date) {
-        if (ano === void 0) { ano = 'unknown'; }
-        if (balance === void 0) { balance = 0; }
-        if (date === void 0) { date = Date.now(); }
-        this.Acc_no = ano;
-        this.Balance = balance;
-        this.Date_of_openning = date;
-    }
-    Account.prototype.addCustomer = function () {
-        console.log("Added a customer");
-    };
-    Account.prototype.removeCustomer = function () {
-        console.log("Removed a customer");
-    };
-    Account.prototype.debitAmount = function () {
-        console.log("Your debit amount");
-    };
-    Account.prototype.creditAmount = function () {
-        console.log("Your credit amount");
-    };
-    Account.prototype.getBalance = function () {
-        return this.Balance;
-    };
-    return Account;
-}());
-var acc1 = new Account("A007", 800000);
+var emp1 = new Employee_1.default(EmployeeData);
+console.log(emp1);
+var man1 = new Manager_1.default(EmployeeData);
+//console.log(man1.GetAddress());
+//!7
+var acc1 = new Account_1.default("A007", 800000);
 console.log(acc1);
 acc1.addCustomer();
 console.log(acc1.getBalance());
 acc1.creditAmount();
-var Saving_Account = /** @class */ (function (_super) {
-    __extends(Saving_Account, _super);
-    function Saving_Account(ano, balance, date) {
-        if (ano === void 0) { ano = 'unknown'; }
-        if (balance === void 0) { balance = 1; }
-        if (date === void 0) { date = Date.now(); }
-        return _super.call(this, ano, balance, date) || this;
-    }
-    return Saving_Account;
-}(Account));
-var Current_Account = /** @class */ (function (_super) {
-    __extends(Current_Account, _super);
-    function Current_Account(ano, balance, date) {
-        if (ano === void 0) { ano = 'unknown'; }
-        if (balance === void 0) { balance = 1; }
-        if (date === void 0) { date = Date.now(); }
-        return _super.call(this, ano, balance, date) || this;
-    }
-    return Current_Account;
-}(Account));
-var acc2 = new Saving_Account("B008", 7004);
+var acc2 = new Saving_Account_1.default("B008", 7004);
 acc2.Min_balance = 500;
+var acc3 = new Current_Account_1.default(7, 50000, 342024);
 console.log(acc2);
+console.log(acc3);
--- lab.ts
+++ lab.ts
@@ -1,3 +1,16 @@
+
+//Main
+
+//!5
+import { Address } from './interfaces/Address';
+import IEmployee from './interfaces/IEmployee';
+//!6
+import Employee from './classes/Employee';
+import Manager from './classes/Manager';
+import IAccount from './interfaces/IAccount';
+import Account from './classes/Account' ;
+import Current_Account from './classes/Current_Account';
+import Saving_Account from './classes/Saving_Account';
 //!1
 let nArray: number[];
 nArray = [3,4,5];
@@ -19,42 +32,6 @@ nboolvar = true ;
 let greeting = (name1: string , name2: string):void => console.log(`hello ${name1} , hello ${name2}`);
 //greeting('aaa','bbb');
 
-//!5
-interface Address{
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-        lat: string;
-        lng: string;
-    };
-}
-interface IEmployee {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
-    address: Address;
-}
-class Employee implements IEmployee {
-    id: number;
-    name: string;
-    private _username: string;
-    email: string;
-    address: Address;
-    constructor(emp: IEmployee){
-        this.id = emp.id;
-        this.name = emp.name;
-        this._username = emp.username;
-        this.email = emp.email;
-        this.address = emp.address;
-    }
-    get username(): string {
-        return this._username;
-    } 
-};
-
 const EmployeeData: IEmployee = {
     id: 1,
     name: "Leanne Graham" ,
@@ -70,74 +47,21 @@ const EmployeeData: IEmployee = {
 }
 
 const emp1 = new Employee(EmployeeData);
-//console.log(emp1);
-
-//!6
-class Manager extends Employee{
-    constructor(manager: IEmployee){
-        super(manager);
-    }
-    GetAddress(): Address{
-        return this.address;
-    }
-}
+console.log(emp1);
 
 const man1 = new Manager(EmployeeData);
 //console.log(man1.GetAddress());
 
 //!7
-interface IAccount{
-    Date_of_openning: number;
-    addCustomer();
-    removeCustomer();
-}
 
-class Account implements IAccount{
-    Acc_no: (string|number);
-    Balance: number;
-    
-    Date_of_openning: number;
-
-    constructor(ano: (string|number) = 'unknown' , balance: number = 0 , date: number = Date.now()){
-        this.Acc_no = ano;
-        this.Balance = balance;
-        this.Date_of_openning = date;
-    }
-
-    addCustomer() {
-        console.log("Added a customer");
-    }
-    removeCustomer() {
-        console.log("Removed a customer");
-    }
-    debitAmount(){
-        console.log("Your debit amount");
-    }
-    creditAmount(){
-        console.log("Your credit amount");
-    }
-    getBalance(){
-        return this.Balance;
-    }
-}
 let acc1 = new Account("A007",800000);
 console.log(acc1);
 acc1.addCustomer();
 console.log( acc1.getBalance() );
 acc1.creditAmount();
 
-class Saving_Account extends Account{
-    Min_balance: number;
-    constructor(ano: (string|number) = 'unknown' , balance: number = 1 , date: number = Date.now()){
-        super(ano,balance,date);
-    }
-}
-class Current_Account extends Account{
-    Interest_rate: number;
-    constructor(ano: (string|number) = 'unknown' , balance: number = 1 , date: number = Date.now()){
-        super(ano,balance,date);
-    }
-}
 let acc2 = new Saving_Account("B008",7004);
 acc2.Min_balance=500;
+let acc3 = new Current_Account(7,50000,342024);
 console.log(acc2);
+console.log(acc3);
